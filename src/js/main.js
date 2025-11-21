565e27dc2bb5887c22d74fc946278c1c9ef2ba31
Ярослаа Филютин <filyutinyaroslav@gmail.com>
2025-11-22 00:26:48 +0300

    Initial publish

--- src/js/main.js
+++ src/js/main.js
@@ -1,5 +1,4 @@
 document.addEventListener('DOMContentLoaded', () => {
-  // Mobile menu toggle
   const hamburger = document.querySelector('.hamburger');
   const navMenu = document.querySelector('.nav-menu');
   
